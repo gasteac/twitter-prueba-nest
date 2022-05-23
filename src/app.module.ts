eb8488a240f61a91bd3b4768ef12883c20aaa40a
Gaston Acosta <gastoneacosta@outlook.com>
2022-05-22 21:23:29 -0300

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirstController } from './first/first.controller';
 
 @Module({
   imports: [], //importa otros modulos
-  controllers: [AppController],
+  controllers: [AppController, FirstController],
   providers: [AppService],
 })
 export class AppModule {}
