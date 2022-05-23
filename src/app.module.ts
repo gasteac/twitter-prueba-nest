68ed7070090d1476384db2b6cec1ab3ea045f6c1
Gaston Acosta <gastoneacosta@outlook.com>
2022-05-22 21:22:56 -0300

    Revert "primer componente"
    This reverts commit a8e7fc27d2003847ea56b1a86c154402f4348ede.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrimconController } from './primcon/primcon.controller';
 
 @Module({
   imports: [], //importa otros modulos
-  controllers: [AppController, PrimconController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/primcon/primcon.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('primcon')
-export class PrimconController {}
