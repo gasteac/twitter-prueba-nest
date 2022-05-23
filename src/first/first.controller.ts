582dd7a2578a36273ff231842be7274e3f8737b3
Gaston Acosta <gastoneacosta@outlook.com>
2022-05-22 23:01:39 -0300

    agregado CRUD

--- src/first/first.controller.ts
+++ src/first/first.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('tuits')
 export class FirstController {
     @Get()
-    getTuits(): string{
-        return 'Hola soy first xd';
+    getTuits(@Query() filterQuery): string{
+        const {searchTerm, orderBy} = filterQuery
+        return `All ${searchTerm} tuits ordered by ${orderBy}`;
     }
+
     @Get(':id')
     getTuit(@Param('id') id:string):string{
         return `tu id es ${id}`
+    }
+
+    @Post()
+    createTuit(@Body("message") message:string): string{
+        return `Your tuit was ${message}`;
+    }
+
+    @Patch(':id')
+    updateTuit(@Param('id') id:string, @Body() tuit):string{
+        return `El tuiter ${id} fue actualizado`;
+    }
+
+    @Delete(':id')
+    deleteTuit(@Param('id') id:string):string{
+        return `El tuiter ${id} fue eliminado`;
     }
 }
